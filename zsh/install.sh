70e113c40a5e2e8ae70f7f7f8b1ab11acbe84ba1
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-14 09:37:57 -0700

    fixed brew install to do without loop

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,7 +4,7 @@ if ! [ -x "$(zsh --version)" ]; then
 fi 
 
 #install brew and flask packages
-for i in $(cat ../brew_packages.txt); do; brew install "$i"; done
-for i in $(cat ../cask_packages.txt); do; brew install "$i"; done
+brew install $(cat ../brew_packages.txt)
+brew install $(cat ../cask_packages.txt)
 
 cp .zsh* ~/
