2451ac5d6e625f65cc251cfe98b78b255460c5e5
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-14 09:34:53 -0700

    updated

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,10 @@
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install brew
+if ! [ -x "$(zsh --version)" ]; then 
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi 
+
+#install brew and flask packages
+for i in $(cat ../brew_packages.txt); do; brew install "$i"; done
+for i in $(cat ../cask_packages.txt); do; brew install "$i"; done
+
 cp .zsh* ~/
