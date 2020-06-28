094c38e8f195e1860de0799b711c81e5050bc58a
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-28 08:14:32 -0700

    Finished install script for zsh config

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,11 +5,15 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 source .zshenv
 mkdir -p "${ZDOTDIR}"
 
-echo "Moving files to HOME: $HOME\n"
+echo "Moving files to HOME: $HOME"
 cp .zshenv "${HOME}"
 cp .vimrc "${HOME}"
 
-echo "Moving files to ZDOTDIR: $ZDOTDIR\n"
+echo "Moving files to ZDOTDIR: $ZDOTDIR"
 cp .zshrc "${ZDOTDIR}"
 cp .zsh_aliases "${ZDOTDIR}"
 ditto .zsh_functions "${ZDOTDIR}/.zsh_functions/"
+
+echo "Completed. Reloading shell..."
+exec zsh
+
