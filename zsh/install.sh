2a8e396510e00b6a66197277ac02a8ff1f59dbeb
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-28 08:08:48 -0700

    Updated install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,15 @@
-# install brew
-if ! [ -x "$(zsh --version)" ]; then 
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi 
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#install brew and flask packages
-brew install $(cat ../brew_packages.txt)
-brew install $(cat ../cask_packages.txt)
+# source this so ZDOTDIR is defined
+souce .zshenv
+#mkdir -p "${ZDOTDIR}/.zsh_custom/"
 
-cp .zsh* ~/
+echo "Moving files to HOME: $HOME\n"
+cp .zshenv "${HOME}"
+cp .vimrc "${HOME}"
+
+echo "Moving files to ZDOTDIR: $ZDOTDIR\n"
+cp .zshrc "${ZDOTDIR}"
+cp .zsh_alises "${ZDOTDIR}"
+ditto .zsh_functions "${ZDOTDIR}/.zsh_functions/"
