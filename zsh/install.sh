28a7974c23b71f64e5535ef48008f46b78129234
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-29 17:46:14 -0700

    Added execute perms to install file

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,5 @@
 # Installs my custom ohmyzsh configuration
+
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -15,6 +16,7 @@ echo "Moving files to ZDOTDIR: $ZDOTDIR"
 cp .zshrc "${ZDOTDIR}"
 cp .zsh_aliases "${ZDOTDIR}"
 ditto .zsh_functions "${ZDOTDIR}/.zsh_functions/"
+chmod -R +x "${ZDOTDIR}/.zsh_functions"
 
 echo "Completed. Reloading shell..."
 exec zsh
